7eddbd998c41a7171f375958585f06877c402a9f
Nhung Nguyen <nhungnguyentester@gmail.com>
2024-10-07 22:25:42 +0700

    2nd

--- OOP/bai-1.py
+++ OOP/bai-1.py
@@ -8,3 +8,19 @@ class Person:
         print(f"Hello {self.name}, Age is {self.age}")
 call = Person("Nhung", 30)
 call.greet()
+
+#Tạo lớp Student kế thừa từ Person và có thêm thuộc tính grade và phương thức study().
+class Student(Person):
+    def __init__(self, name, age, grade):
+        super().__init__(name, age)
+        self.grade = grade
+
+    def study(self):
+        print(f"Hoc sinh ten {self.name}, Tuoi: {self.age}, co diem thi: {self.grade}")
+
+call = Student("Nhung", 30, 9) 
+# call.greet
+call.study()
+
+
+        
---
+++ OOP/bai-2.py
@@ -0,0 +1,3 @@
+#Tạo lớp Student kế thừa từ Person và có thêm thuộc tính grade và phương thức study().
+import OOP
+class Student(bai-1.Person)
