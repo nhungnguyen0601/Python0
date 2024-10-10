d3001d4a6cc6ae404d2eb525d8780edcb1ed23f2
Nhung Nguyen <nhungnguyentester@gmail.com>
2024-10-10 22:01:19 +0700

    10 oct

--- OOP/bai-2.py
+++ OOP/bai-2.py
@@ -1,3 +1,8 @@
 # #Tạo lớp Student kế thừa từ Person và có thêm thuộc tính grade và phương thức study().
-# import OOP
+# import 
 # class Student(bai-1.Person)
+from bai import Person
+class Studenta(Person):
+    def __init__(self, name, age, grade):
+        super().__init__(name, age)
+        self.grade = grade
--- OOP/bai.py
+++ OOP/bai.py
@@ -6,20 +6,22 @@ class Person:
     
     def greet(self):
         print(f"Hello {self.name}, Age is {self.age}")
-call = Person("Nhung", 30)
-call.greet()
+# call = Person("Nhung", 30)
+# call.greet()
 
 #Tạo lớp Student kế thừa từ Person và có thêm thuộc tính grade và phương thức study().
-class Student(Person):
+class Student():
     def __init__(self, name, age, grade):
-        super().__init__(name, age)
+        # super().__init__(name, age)
+        self.name = name
+        self.age = age
         self.grade = grade
 
     def study(self):
         print(f"Hoc sinh ten: {self.name}, Tuoi: {self.age}, co diem thi: {self.grade}")
 #Tạo một đối tượng Student và gọi các phương thức từ cả lớp cha và lớp con.
 student1 = Student("Nhung", 30, 9) 
-student1.greet()
+# student1.greet()
 student1.study()
 
 
